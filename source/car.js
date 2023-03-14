5f3be74d00fd53a40c802c9e1aa1a2216065740d
Yoane Yatalathov <yatalathov.gamedev@gmail.com>
2023-03-14 22:55:18 +0700

    membuat object Fiat

--- source/car.js
+++ source/car.js
@@ -17,3 +17,13 @@ let cadi = {
     convertible: false,
     mileage: 12.892
 };
+
+let fiat = {
+    make: "Fiat",
+    model: "500",
+    year: 1957,
+    color: "Medium Blue",
+    passenger: 2,
+    convertible: false,
+    mileage: 88000
+};
